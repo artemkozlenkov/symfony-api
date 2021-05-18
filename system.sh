d9472922ee6b7ddd5e84adfd7fd9913eee86866f
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-05-19 00:19:58 +0200

    init

---
+++ system.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e	 
+
+	if [ $1 == 'up' ] | [ $1 == 'down' ];then 
+	DCMP=docker-compose
+
+	CMD=$1
+	shift;
+	$DCMP $CMD --remove-orphans "$@"
+	fi;
+
+	if [ $1 == 'exec' ];then
+	ARG=$1
+	shift;
+	docker $ARG -it "$@" bash
+	fi;
