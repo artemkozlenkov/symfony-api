c67b766cb8763c95051c4034b1e16fdd5f59e84b
artem kozlenkov <artem.kozlenkov@gmail.com>
2021-05-19 19:52:15 +0200

    multiple updates

--- system.sh
+++ system.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e	 
 
-if [ $1 == 'up' ] || [ $1 == 'down' ];then
+if [ $1 == 'up' ] || [ $1 == 'stop' ];then
 	DCMP=docker-compose
 
 	CMD=$1
 	shift;
-	$DCMP $CMD --remove-orphans "$@"
+	$DCMP $CMD "$@"
 	exit;
 fi;
 
@@ -13,4 +13,16 @@ if [ $1 == 'exec' ];then
 	ARG=$1
 	shift;
 	docker $ARG -it "$@" bash
+fi;
+
+if [ $1 == 'start' ]; then
+/bin/bash $(pwd)/system.sh up 
+until [ "`docker inspect -f {{.State.Health.Status}} api`"=="starting" ]; do
+    sleep 0.1;
+done;
+
+sleep 10;
+
+bin/console composer install 
+
 fi;
